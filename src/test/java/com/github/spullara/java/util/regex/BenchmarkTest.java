849385c8ad3bec309086557cbc25ecc0965b55d7
Sam Pullara <spullara@twitter.com>
2011-10-20 18:00:18 -0700

    check to make sure that we are getting the same results

--- src/test/java/com/github/spullara/java/util/regex/BenchmarkTest.java
+++ src/test/java/com/github/spullara/java/util/regex/BenchmarkTest.java
@@ -5,6 +5,8 @@ import java.util.Random;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 public class BenchmarkTest {
     private static String LATIN_ACCENTS_CHARS = "\\u00c0-\\u00d6\\u00d8-\\u00f6\\u00f8-\\u00ff\\u015f";
     private static final String HASHTAG_ALPHA_CHARS = "a-z" + LATIN_ACCENTS_CHARS +
@@ -21,13 +23,13 @@ public class BenchmarkTest {
 
     @Test
     public void testSpeed() {
-        test(Pattern.compile("[a-z]"));
-        test(java.util.regex.Pattern.compile("[a-z]"));
-        test(Pattern.compile("[" + HASHTAG_ALPHA_CHARS + "]"));
-        test(java.util.regex.Pattern.compile("[" + HASHTAG_ALPHA_CHARS + "]"));
+        int base = test(java.util.regex.Pattern.compile("[a-z]"));
+        assertEquals(base, test(Pattern.compile("[a-z]")));
+        base = test(java.util.regex.Pattern.compile("[" + HASHTAG_ALPHA_CHARS + "]"));
+        assertEquals(base, test(Pattern.compile("[" + HASHTAG_ALPHA_CHARS + "]")));
     }
 
-    private void test(Pattern p) {
+    private int test(Pattern p) {
         Random random = new Random(239487234);
         String[] toMatch = new String[100];
         char[] chars = "abcdefghijklmnopqrstuvwxyz ".toCharArray();
@@ -40,15 +42,19 @@ public class BenchmarkTest {
             toMatch[i] = sb.toString();
         }
         long start = System.currentTimeMillis();
+        int total = 0;
         for (int i = 0; i < TIMES; i++) {
             Matcher matcher = p.matcher(toMatch[i % 100]);
-            while (matcher.find());
+            while (matcher.find()) {
+                total++;
+            }
         }
         long diff = System.currentTimeMillis() - start;
         System.out.println(TIMES /diff);
+        return total;
     }
 
-    private void test(java.util.regex.Pattern p) {
+    private int test(java.util.regex.Pattern p) {
         Random random = new Random(239487234);
         String[] toMatch = new String[100];
         char[] chars = "abcdefghijklmnopqrstuvwxyz ".toCharArray();
@@ -61,12 +67,16 @@ public class BenchmarkTest {
             toMatch[i] = sb.toString();
         }
         long start = System.currentTimeMillis();
+        int total = 0;
         for (int i = 0; i < TIMES; i++) {
             java.util.regex.Matcher matcher = p.matcher(toMatch[i % 100]);
-            while (matcher.find());
+            while (matcher.find()) {
+                total++;
+            }
         }
         long diff = System.currentTimeMillis() - start;
         System.out.println(TIMES /diff);
+        return total;
     }
 
 }
